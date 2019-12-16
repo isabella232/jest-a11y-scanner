896ae45cd0c484cdeba1315dc26025cd2a076765
okry <alexanderlokros@gmail.com>
2019-12-16 09:30:36 -0500

    feat: add file write on error

---
+++ __tests__/telus-example.test.js
@@ -0,0 +1,68 @@
+const { configureAxe, axe, toHaveNoViolations, toAuditNoViolations, reportViolations } = require('../index.js')
+
+expect.extend(toAuditNoViolations)
+expect.extend(toHaveNoViolations)
+
+describe("Telus test series", () => {
+  it('should log output, but never error', async () => {
+    const render = () => `
+      <div>
+        <img src="#"/>
+      </div>
+    `
+
+    // pass anything that outputs html to axe
+    const html = render()
+
+    const results = await axe(html);
+
+    expect(results).toAuditNoViolations()
+  })
+  it.skip('should error and log output', async () => {
+    const render = () => `
+      <div>
+        <img src="#"/>
+      </div>
+    `
+
+    // pass anything that outputs html to axe
+    const html = render()
+
+    const results = await axe(html);
+
+    expect(results).toHaveNoViolations()
+  })
+  it('should report violations', async () => {
+    const render = () => `
+      <div>
+        <img src="#"/>
+      </div>
+    `
+
+    // pass anything that outputs html to axe
+    const html = render()
+
+    const results = await axe(html);
+
+    reportViolations(results, "./report.txt");
+  })
+  it('should have empty report violations', async () => {
+    const render = () => `
+      <div>
+        <img src="#"/>
+      </div>
+    `
+
+    // pass anything that outputs html to axe
+    const html = render()
+
+    const results = await axe(html, {
+      rules: {
+        // for demonstration only, don't disable rules that need fixing.
+        'image-alt': { enabled: false }
+      }
+    })
+
+    reportViolations(results, "./empty_report.txt");
+  })
+})
--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const axeCore = require('axe-core')
 const merge = require('lodash.merge')
 const chalk = require('chalk')
+const fs = require('fs')
 const { printReceived, matcherHint } = require('jest-matcher-utils')
 
 /**
@@ -142,8 +143,123 @@ const toHaveNoViolations = {
   }
 }
 
+/**
+ * Custom Jest expect matcher, that can check aXe results for violations.
+ * This matcher will never cause a test to fail. It simply consoles out the error if one exists.
+ * @param {object} object requires an instance of aXe's results object
+ * (https://github.com/dequelabs/axe-core/blob/develop-2x/doc/API.md#results-object)
+ * @returns {object} returns Jest matcher object
+ */
+const toAuditNoViolations = {
+  toAuditNoViolations (results) {
+    const violations = results.violations
+
+    if (typeof violations === 'undefined') {
+      throw new Error('No violations found in aXe results object')
+    }
+
+    const reporter = violations => {
+      if (violations.length === 0) {
+        return []
+      }
+
+      const lineBreak = '\n\n'
+      const horizontalLine = '\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500'
+
+      return violations.map(violation => {
+        const errorBody = violation.nodes.map(node => {
+          const selector = node.target.join(', ')
+          const expectedText = `Expected the HTML found at $('${selector}') to have no violations:` + lineBreak
+          return (
+            expectedText +
+            chalk.grey(node.html) +
+            lineBreak +
+            `Received:` +
+            lineBreak +
+            printReceived(`${violation.help} (${violation.id})`) +
+            lineBreak +
+            chalk.yellow(node.failureSummary) +
+            lineBreak +
+            `You can find more information on this issue here: \n` +
+            chalk.blue(violation.helpUrl)
+          )
+        }).join(lineBreak)
+
+        return (errorBody)
+      }).join(lineBreak + horizontalLine + lineBreak)
+    }
+
+    const formatedViolations = reporter(violations)
+    const pass = true;
+
+    const message = () => {
+      return matcherHint('.toAuditNoViolations') +
+            '\n\n' +
+            `${formatedViolations}`
+    }
+
+    console.log(message());
+    return { actual: violations, message, pass }
+  }
+}
+
+/**
+ * Writes out violation results to a text file
+ * @param {object} results requires an instance of aXe's results object
+ * (https://github.com/dequelabs/axe-core/blob/develop-2x/doc/API.md#results-object)
+ * @param {string} path path string to where you want your file to be written to.
+ */
+function reportViolations (results, path) {
+  const violations = results.violations
+
+  if (typeof violations === 'undefined') {
+    throw new Error('No violations found in aXe results object')
+  }
+
+  const reporter = violations => {
+    if (violations.length === 0) {
+      return []
+    }
+
+    const lineBreak = '\n\n'
+    const horizontalLine = '\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500'
+
+    return violations.map(violation => {
+      const errorBody = violation.nodes.map(node => {
+        const selector = node.target.join(', ')
+        const expectedText = `Expected the HTML found at $('${selector}') to have no violations:` + lineBreak
+        return (
+          expectedText +
+          node.html +
+          lineBreak +
+          `Received:` +
+          lineBreak +
+          `${violation.help} (${violation.id})` +
+          lineBreak +
+          node.failureSummary +
+          lineBreak +
+          `You can find more information on this issue here: \n` +
+          violation.helpUrl
+        )
+      }).join(lineBreak)
+
+      return (errorBody)
+    }).join(lineBreak + horizontalLine + lineBreak)
+  }
+
+  const formatedViolations = reporter(violations)
+
+  let message = `${formatedViolations}`
+  if (message.length === 0) message = `No violations found!`
+
+  fs.writeFile(path, message, () => {})
+}
+
+
 module.exports = {
   configureAxe,
   axe: configureAxe(),
-  toHaveNoViolations
+  toHaveNoViolations,
+  toAuditNoViolations,
+  reportViolations
 }
